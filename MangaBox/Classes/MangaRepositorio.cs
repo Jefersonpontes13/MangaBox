2fe177e5b9b6a1c0961eb1a69945b7387e43e24a
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-17 12:32:40 -0300

    Inicia Interfaces e Repositórios
    Bootcamp DIO GFT .NET #2
    Cria IRepositorio.cs
    Cria MangaRepositorio.cs

---
+++ MangaBox/Classes/MangaRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using MangaBox.Interfaces;
+
+namespace MangaBox.Classes
+{
+    public class MangaRepositorio : IRepositorio<Manga>
+    {
+
+        private List<Manga> listaManga = new List<Manga>();
+        
+        public List<Manga> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Manga RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Manga entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualiza(int id, Manga entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
